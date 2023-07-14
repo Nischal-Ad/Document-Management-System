dcb22a75a67b2816e580afefbf9dcce3fca86f79
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-07-14 22:51:56 +0545

    bootstrap working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import store from './store/store.jsx';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const index = () => {
-	return <div>index</div>;
+	return <>
+	<div class="card" >
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
+	</>;
 };
 
 export default index;
