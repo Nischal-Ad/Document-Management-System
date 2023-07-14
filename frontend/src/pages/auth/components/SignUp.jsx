b242d418d01616f53d56efcb939a3c3481cfb207
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-07-14 13:07:09 +0545

    auth page

--- frontend/src/pages/auth/components/SignUp.jsx
+++ frontend/src/pages/auth/components/SignUp.jsx
@@ -1,14 +1,14 @@
 import { Box, Button, Stack, TextField } from '@mui/material';
 const SignUp = () => {
 	return (
-		<Stack width={{ md: '30%', sm: '60%', xs: '100%' }} spacing={3}>
+		<Stack width={{ md: '100%', sm: '100%', xs: '100%' }} spacing={3}>
 			<TextField type='email' label='email' variant='standard' />
 			<TextField type='password' label='password' variant='standard' />
 			<Box display={'flex'} justifyContent={'center'}>
 				<Button
 					variant='contained'
 					sx={{
-						width: '14rem',
+						width: '10rem',
 					}}
 				>
 					Sign Up
--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,43 +1,51 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, Card, CardMedia, CardContent } from '@mui/material';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import { useState } from 'react';
+import logo from '../../assects/img/logo.png';
 
 const Index = () => {
 	const [isLogin, setIsLogin] = useState(true);
 	return (
 		<>
-			<Stack alignItems={'center'} spacing={3}>
-				<>
-					<Typography variant='h3' textAlign={'center'}>
-						Hey Welcome to my Website
-					</Typography>
-					<img
-						src='https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg'
-						alt=''
-						style={{
-							height: '10rem',
-							width: '10rem',
-							borderRadius: '5rem',
-						}}
-					/>
-					<Typography variant='h3' textAlign={'center'}>
-						{isLogin ? 'Login' : 'Sign Up'}
-					</Typography>
-					{isLogin ? <Login /> : <SignUp />}
-					<Typography
-						onClick={() => setIsLogin(!isLogin)}
-						variant='body1'
-						mt={2}
-						color={'blueviolet'}
-						sx={{
-							cursor: 'pointer',
-						}}
-					>
-						{isLogin ? "Don't have Alc? Sign Up" : 'Already have an Alc? Login'}
-					</Typography>
-				</>
-			</Stack>
+			<Card sx={{ maxWidth: 345 }}>
+				<Stack alignItems={'center'} spacing={3}>
+					<Stack>
+						<Typography variant='h4' fontStyle={'italic'} textAlign={'center'}>
+							Hey Welcome
+						</Typography>
+
+						<CardMedia>
+							<img
+								src={logo}
+								alt=''
+								style={{
+									height: '7rem',
+									width: '7rem',
+									borderRadius: '5rem',
+								}}
+							/>
+						</CardMedia>
+						<CardContent>
+							<Typography variant='h3' textAlign={'center'}>
+								{isLogin ? 'Login' : 'Sign Up'}
+							</Typography>
+							{isLogin ? <Login /> : <SignUp />}
+							<Typography
+								onClick={() => setIsLogin(!isLogin)}
+								variant='body1'
+								mt={2}
+								color={'blueviolet'}
+								sx={{
+									cursor: 'pointer',
+								}}
+							>
+								{isLogin ? "Don't have Alc? Sign Up" : 'Already have an Alc? Login'}
+							</Typography>
+						</CardContent>
+					</Stack>
+				</Stack>
+			</Card>
 		</>
 	);
 };
