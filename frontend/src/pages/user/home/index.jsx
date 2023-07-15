7aa9044441cef93bfadc5c70cfa6907e004f671b
Sujit <sujitshrestha665@gmail.com>
2023-07-15 21:24:28 +0545

    message

--- frontend/src/pages/user/home/index.jsx
+++ frontend/src/pages/user/home/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const index = () => {
-  return <div>this is home page for user</div>;
+  return (
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+      <div class="card">
+        <div class="card-body"></div>
+        <h5 class="card-title">All documents</h5>
+      </div>
+      <div
+        className="rectangle"
+        style={{ width: 1082, height: 380, background: "#D9D9D9" }}
+      >
+        <h3 className="text-center" style={{ fontWeight: "700" }}>
+          Latest Documents
+        </h3>
+      </div>
+    </>
+  );
 };
 
 export default index;
--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -1,34 +1,42 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import ProtectedRoute from './ProtectedRoute';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 
 //normal components
-import Footer from '../shared/footer/Footer';
-import Navbar from '../shared/navbar/UserNav';
-import Home from '../pages/user/home';
-import Documents from '../pages/user/document/view document';
-import AddDocument from '../pages/user/document/add document';
-import ViewDocument from '../pages/user/document/view document/Details';
-import Error from '../shared/Error';
-import Auth from '../pages/auth';
+import Footer from "../shared/footer/Footer";
+import Navbar from "../shared/navbar/UserNav";
+import Home from "../pages/user/home";
+import Documents from "../pages/user/document/view document";
+import AddDocument from "../pages/user/document/add document";
+import ViewDocument from "../pages/user/document/view document/Details";
+import Error from "../shared/Error";
+import Auth from "../pages/auth";
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<>
-			{/* normal routes  */}
-			<Route path='/' element={<Auth />} />
-			<Route path='*' element={<Error />} />
-			{/* end of normal routes  */}
+  createRoutesFromElements(
+    <>
+      {/* normal routes  */}
+      <Route path="/" element={<Auth />} />
+      <Route path="*" element={<Error />} />
+      {/* end of normal routes  */}
 
-			{/* ProtectedRoute for users  */}
-			<Route element={<ProtectedRoute isAuth={true} nav={<Navbar />} footer={<Footer />} />}>
-				<Route path='/home' element={<Home />} />
-				<Route path='/user/documents' element={<Documents />} />
-				<Route path='/user/add-document' element={<AddDocument />} />
-				<Route path='/user/documents/:id' element={<ViewDocument />} />
-			</Route>
-			{/* end of ProtectedRoute for users  */}
-		</>
-	)
+      {/* ProtectedRoute for users  */}
+      <Route
+        element={
+          <ProtectedRoute isAuth={true} nav={<Navbar />} footer={<Footer />} />
+        }
+      >
+        <Route path="/home" element={<Home />} />
+        <Route path="/user/documents" element={<Documents />} />
+        <Route path="/user/add-document" element={<AddDocument />} />
+        <Route path="/user/documents/:id" element={<ViewDocument />} />
+      </Route>
+      {/* end of ProtectedRoute for users  */}
+    </>
+  )
 );
 
 export default router;
