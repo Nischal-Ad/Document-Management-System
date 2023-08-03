546f23c345e9e765ba996dcf036dc0f7cec66e8d
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-08-04 00:04:42 +0545

    setup tailwind in react native

--- mobile-app/app/index.js
+++ mobile-app/app/index.js
@@ -1,36 +1,12 @@
-import { Link } from 'expo-router';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import React from 'react';
 
-export default function Page() {
+const index = () => {
 	return (
-		<View style={styles.container}>
-			<View style={styles.main}>
-				<Text style={styles.title}>Hello World</Text>
-				<Text style={styles.subtitle}>This is the first page of your app.</Text>
-				<Link href={'/UserHome'}> Go to user home</Link>
-			</View>
+		<View>
+			<Text>index</Text>
 		</View>
 	);
-}
+};
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		padding: 24,
-	},
-	main: {
-		flex: 1,
-		justifyContent: 'center',
-		maxWidth: 960,
-		marginHorizontal: 'auto',
-	},
-	title: {
-		fontSize: 64,
-		fontWeight: 'bold',
-	},
-	subtitle: {
-		fontSize: 36,
-		color: '#38434D',
-	},
-});
+export default index;
