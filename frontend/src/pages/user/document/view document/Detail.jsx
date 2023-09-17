44a870fbde1e01acb685f5f358d1719d70e80877
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-09-17 13:40:29 +0545

    img fix

--- frontend/src/pages/user/document/view document/Detail.jsx
+++ frontend/src/pages/user/document/view document/Detail.jsx
@@ -7,9 +7,9 @@ const Detail = ({ open, close, data }) => {
 	return (
 		<Modal show={open} onClose={() => close()}>
 			<Modal.Header>{data?.name}</Modal.Header>
-			<Modal.Body style={{ overflow: 'hidden' }}>
+			<Modal.Body style={{ overflow: 'auto' }}>
 				{imageExtensions.includes(lastWord) ? (
-					<img src={data?.doc?.url} className='w-[100%] object-fill' style={{ height: '90dvh' }} />
+					<img src={data?.doc?.url} className='w-[100%] object-none' />
 				) : (
 					<iframe src={`https://docs.google.com/gview?url=${data?.doc?.url}&embedded=true`} className='w-[100%]' style={{ height: '90dvh' }} />
 				)}
