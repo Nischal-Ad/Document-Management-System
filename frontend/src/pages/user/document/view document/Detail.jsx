87399ba3504175e1c81e4c948bee36c5212897e6
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-09-17 13:31:26 +0545

    display img in doc

--- frontend/src/pages/user/document/view document/Detail.jsx
+++ frontend/src/pages/user/document/view document/Detail.jsx
@@ -1,11 +1,18 @@
 import { Modal } from 'flowbite-react';
 
 const Detail = ({ open, close, data }) => {
+	const parts = data?.doc?.url?.split('.');
+	const lastWord = parts[parts.length - 1].toLowerCase();
+	const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'jfif'];
 	return (
 		<Modal show={open} onClose={() => close()}>
 			<Modal.Header>{data?.name}</Modal.Header>
 			<Modal.Body style={{ overflow: 'hidden' }}>
-				<iframe src={`https://docs.google.com/gview?url=${data?.doc?.url}&embedded=true`} className='w-[100%]' style={{ height: '90dvh' }} />
+				{imageExtensions.includes(lastWord) ? (
+					<img src={data?.doc?.url} className='w-[100%]' style={{ height: '90dvh' }} />
+				) : (
+					<iframe src={`https://docs.google.com/gview?url=${data?.doc?.url}&embedded=true`} className='w-[100%]' style={{ height: '90dvh' }} />
+				)}
 			</Modal.Body>
 			<Modal.Footer>{data?.desc}</Modal.Footer>
 		</Modal>
